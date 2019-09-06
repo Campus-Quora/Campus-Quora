4e1f85657b89b8100cad1a275f5c0356f2882373
Harsh Motwani <harsh18@iitg.ac.in>
2019-09-06 23:32:52 +0530

    Initial Commit

---
+++ TheCampusApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheCampusApp
+//
+//  Created by Harsh Motwani on 06/09/19.
+//  Copyright © 2019 Harsh Motwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
